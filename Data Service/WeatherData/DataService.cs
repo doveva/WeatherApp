c9494f6ae0355c1e12dad19eb9aeb727fe285806
doveva <doveva@mail.ru>
2022-12-25 21:30:13 +0300

    Merge conflict fix

--- Data Service/WeatherData/DataService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DotNetWeatherData.WeatherData;
-
-public class DataService<T> where T: BaseWeatherData
-{
-    public string ServiceName { get; set; }
-    public string Place { get; set; }
-    public T[] Data { get; set; }
-    public DateTime Created { get; set; }
-}
